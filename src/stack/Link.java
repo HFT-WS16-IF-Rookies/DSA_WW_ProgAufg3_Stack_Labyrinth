4ef62b718e8b575b3539aca0bdb9f6dd174ea3c1
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-11-07 10:12:29 +0100

    autoformat stack.Link

--- src/stack/Link.java
+++ src/stack/Link.java
@@ -14,26 +14,28 @@
  * 
  * ------------------------------------------------*/
 
-/*
+ /*
  * Klasse zur Realisierung einer verketteten Listen;
  * "data" nimmt die Datenelement auf; 
  * "next" verweist auf das nächste Element der Liste oder ist "null"
  */
-
 package stack;
 
-public class Link {
-	
-	protected Object data;
-	protected Link next;
-	
-	public Link(){				// Erzeugt ein neues, leeres Link-Objekt
-		this.data = null;
-		this.next = null;
-	}
-	
-	public Link(Object data){	// Erzeugt ein neues, gefülltes Link-Objekt
-		this.data = data;
-		this.next = null;
-	}
+public class Link
+{
+
+    protected Object data;
+    protected Link next;
+
+    public Link()
+    {				// Erzeugt ein neues, leeres Link-Objekt
+        this.data = null;
+        this.next = null;
+    }
+
+    public Link(Object data)
+    {	// Erzeugt ein neues, gefülltes Link-Objekt
+        this.data = data;
+        this.next = null;
+    }
 }

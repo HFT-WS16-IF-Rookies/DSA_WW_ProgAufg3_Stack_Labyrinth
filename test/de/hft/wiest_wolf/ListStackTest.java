f6eba6fdfe5e1c0f05d55c03109f773b95fed9b7
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-11-07 07:45:20 +0100

    add JUnit Test for ListStack

---
+++ test/de/hft/wiest_wolf/ListStackTest.java
@@ -0,0 +1,103 @@
+package de.hft.wiest_wolf;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import stack.*;
+
+/**
+ *
+ * @author Lukas Wiest
+ */
+public class ListStackTest
+{
+    @Test
+    public void testVersion()
+    {
+        Stack instance = new ListStack();
+        assertEquals(instance.version(), "Version 1.b - Stack; Implementierung als verkettete Liste mit Exception-Handling");
+    }
+
+    @Test
+    public void testEmpty()
+    {
+        Stack instance = new ListStack();
+        instance.push("ein Element");
+        instance.empty();
+        assert(instance.size() == 0);
+    }
+
+    @Test
+    public void testPush()
+    {
+        Stack instance = new ListStack();
+        String expected = "ein Element";
+        instance.push(expected);
+        assertEquals(expected, instance.peek());
+    }
+
+    @Test
+    public void testPop()
+    {
+        Stack instance = new ListStack();
+        String expected = "ein Element";
+        instance.push(expected);
+        assert(instance.size() == 1);
+        assertEquals(expected, instance.pop());
+        assert(instance.size() == 0);
+    }
+
+    @Test
+    public void testSize()
+    {
+        Stack instance = new ListStack();
+        instance.push("erstes Element");
+        instance.push(15);
+        instance.push("drittes Elemen");
+        instance.pop();
+        assert(instance.size() == 2);
+    }
+
+    @Test
+    public void testIsEmpty()
+    {
+        Stack instance = new ListStack();
+        assertTrue(instance.isEmpty());
+    }
+
+    @Test
+    public void testIsFull()
+    {
+        Stack instance = new ListStack();
+        assertFalse(instance.isFull());
+    }
+
+    @Test
+    public void testPeek()
+    {
+        Stack instance = new ListStack();
+        String expected = "ein Element";
+        instance.push(expected);
+        assertEquals(expected, instance.peek());
+        assert(instance.size() == 1);
+    }
+
+    @Test
+    public void testToString()
+    {
+        Stack instance = new ListStack();
+        String first = "ein Element";
+        String second = "nochmal";
+        int third = 15;
+        String fourth = "letztes";
+
+        String expected = first + " " + second + " " + third + " [" + fourth + "]";
+
+        instance.push(first);
+        instance.push(second);
+        instance.push(third);
+        instance.push(fourth);
+
+        assertEquals(expected, instance.toString());
+    }
+}

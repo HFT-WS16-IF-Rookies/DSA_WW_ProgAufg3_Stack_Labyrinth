5ac296c43ae910f8b3c173546d46b1d130147aac
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-11-08 08:59:54 +0100

    add first part for labyrinth task

---
+++ src/de/hft/wiest_wolf/Labyrinth.java
@@ -0,0 +1,16 @@
+package de.hft.wiest_wolf;
+
+/**
+ *
+ * @author Lukas Wiest
+ * @author Erik Wolf
+ */
+public class Labyrinth
+{
+    Feld[] data;
+
+    public class Feld
+    {
+        
+    }
+}

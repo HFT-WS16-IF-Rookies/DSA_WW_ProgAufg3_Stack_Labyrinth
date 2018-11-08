e13aeb373e78b526bf455c213b902921a0e82abb
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-11-08 16:50:42 +0100

    autoformat to get rid of tabstops

--- src/de/hft/wiest_wolf/ArrayStack.java
+++ src/de/hft/wiest_wolf/ArrayStack.java
@@ -9,80 +9,91 @@ import stack.StackFullException;
  */
 public class ArrayStack extends stack.Stack
 {
-	private Object[] stack = new Object[50];
-	int top = 0;
+
+    private Object[] stack = new Object[50];
+    int top = 0;
 
     @Override
     public String version()
     {
-    	return "Aufgabe 1.a - Stack; Implementierung als Array mit Exception-Handling";
+        return "Aufgabe 1.a - Stack; Implementierung als Array mit Exception-Handling";
     }
 
     @Override
     public void empty()
     {
-    	stack = new Object [50];
+        stack = new Object[50];
         top = 0;
     }
 
     @Override
     public void push(Object element) throws StackFullException
     {
-    	if(top >= stack.length)
-    		throw new StackFullException();
-		stack[top++] = element;
+        if (top >= stack.length)
+        {
+            throw new StackFullException();
+        }
+        stack[top++] = element;
     }
 
     @Override
     public Object pop() throws StackEmptyException
     {
-    	if(top < 1)
-    		throw new StackEmptyException();
-		Object tmp= stack[--top];
-		stack[top] = null;
-		return tmp;
+        if (top < 1)
+        {
+            throw new StackEmptyException();
+        }
+        Object tmp = stack[--top];
+        stack[top] = null;
+        return tmp;
     }
 
     @Override
     public int size()
     {
-    	return top;
+        return top;
     }
 
     @Override
     public boolean isEmpty()
     {
-    	if(top>0)
-    		return false;
-    	return true;
+        if (top > 0)
+        {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean isFull()
     {
-    	if(top >= stack.length)
-    		return true;
-    	return false;
+        if (top >= stack.length)
+        {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public Object peek() throws StackEmptyException
     {
-    	if(top < 1)
-    		throw new StackEmptyException();
-    	return stack[top-1];
+        if (top < 1)
+        {
+            throw new StackEmptyException();
+        }
+        return stack[top - 1];
     }
 
     @Override
     public String toString()
     {
-    	String tmp = "";
-    	for(int i = 0; i < top-1; i++)
-    	{
-    		tmp += stack[i] + " ";
-    	}
-    	tmp +="["+stack[top - 1] + "]";
-    	return tmp;
+        String tmp = "";
+        for (int i = 0; i < top - 1; i++)
+        {
+            tmp += stack[i] + " ";
+        }
+        tmp += "[" + stack[top - 1] + "]";
+        return tmp;
     }
-    
+
 }
